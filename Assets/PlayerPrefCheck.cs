408a2a059d826ddc2fbc5f74f886a5c9c4537f3f
blueberry-lees <nalees5789@gmail.com>
2025-05-27 13:02:15 +1200

    Added playerPrefCheck

---
+++ Assets/PlayerPrefCheck.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefCheck : MonoBehaviour
+{
+
+    public static PlayerPrefCheck instance;
+
+
+    [Header("Inspect only")]
+
+    public string playerName;
+    public int hasPlayedBefore;
+    public  string inkStateJSON = "";
+
+
+    [Header("Last tags in ink")]
+    public string lastBackground = "";
+    public string lastCharacter = "";
+    public string lastExpression = "";
+    public string lastSpeaker = "";
+
+    [Header("Where the next knot to go when enter story?")]
+    public string returnPoint = "";
+
+    [Header("What version should the Interface be?")]
+    public int uiVersion = 1;
+
+    [Header("Reset player prefs on start?")]
+    public bool isPlyaerPrefsCleared = false;
+
+
+
+    private void Awake()
+    {
+
+        //IF AN INSTANCE ALREADY EXIST AND IT'S NOT THIS ONE, DESTORY THIS OBJ
+        if(instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+        //SET THE INSTANCE AND MARK AS PERSISTENT
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        ResetPlayerPrefOnStart();
+    }
+    void Start()
+    {
+        UpdatePlayerPrefCheck();
+    }
+
+
+
+    void Update()
+    {
+        UpdatePlayerPrefCheck();
+    }
+
+
+
+    public void UpdatePlayerPrefCheck()
+    {
+        playerName =  PlayerPrefs.GetString("playerName", "");
+        hasPlayedBefore = PlayerPrefs.GetInt("hasPlayedBefore", 0);
+        inkStateJSON = PlayerPrefs.GetString("InkState", "");
+        lastBackground = PlayerPrefs.GetString("LastBackground", "");
+        lastCharacter = PlayerPrefs.GetString("LastCharacter", "");
+        lastExpression = PlayerPrefs.GetString("LastExpression", "");
+        lastSpeaker = PlayerPrefs.GetString("LastSpeaker", "");
+        returnPoint = PlayerPrefs.GetString("ReturnPoint", "");
+        uiVersion = PlayerPrefs.GetInt("UIVersion", 1);
+    }
+
+
+    public void ResetPlayerPrefOnStart()
+    {
+
+        if (isPlyaerPrefsCleared)
+        {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
+            isPlyaerPrefsCleared = false;
+        }
+        else return;
+        
+    }
+
+
+}
