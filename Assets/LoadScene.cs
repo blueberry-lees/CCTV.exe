5c238b7c4532fe05aa84325eb2f7471ee4f22ece
blueberrylees <nalees5789@gmail.com>
2025-06-13 01:51:51 +1200

    ok now focus on finish the story

--- Assets/LoadScene.cs
+++ Assets/LoadScene.cs
@@ -19,6 +19,8 @@ public class LoadScene : MonoBehaviour
     }
 
 
+
+
     public void LoadTheSceneAdditive(string sceneName)
     {
         SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
@@ -31,5 +33,34 @@ public class LoadScene : MonoBehaviour
             GameState.ClearDialogueHistory();
         }
         else return;
+    }
+
+    public void LoadInterfaceScene()
+    {
+        switch (GameState.uiVersion)
+        {
+            case 1:
+                Debug.Log("UIVersion 1 ? Version1");
+                SceneManager.LoadScene("Version1");
+                break;
+            case 2:
+                Debug.Log("UIVersion 2 ? Version2");
+                SceneManager.LoadScene("Version2");
+
+                break;
+            case 3:
+                Debug.Log("UIVersion 3 ? Version3");
+                SceneManager.LoadScene("Version3");
+
+                break;
+            case 4:
+                Debug.Log("UIVersion 4 ? Version4");
+                GameState.returnPoint = "SPLIT_ENDING"; //rename
+                SceneManager.LoadScene("Version4");
+                break;
+            default:
+                Debug.LogWarning("Unknown UI Version");
+                break;
+        }
     }
 }
