762fa4094553a4352c86fda46bb5f00b37327df8
blueberrylees <nalees5789@gmail.com>
2025-06-14 12:36:41 +1200

    added toggle full screen

--- Assets/ToggleFullScreen.cs
+++ Assets/ToggleFullScreen.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class ToggleFullScreen : MonoBehaviour
 {
-    public void FullScreen()
+    private bool isFullscreen = false;
+    private int targetWidth = 720;
+    private int targetHeight = 720;
+
+    void Update()
     {
-        
-            if (Screen.fullScreen)
-            {
-                Screen.SetResolution(512, 512, false);
-            }
-            else
-            {
-                Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
-            }
+        if (Input.GetKeyDown(KeyCode.F4))
+        {
+            ToggleFullscreen();
         }
-    
+    }
+
+    void ToggleFullscreen()
+    {
+        isFullscreen = !isFullscreen;
+
+        if (isFullscreen)
+        {
+            // Go fullscreen at current monitor resolution but maintain the 1:1 aspect
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+
+            // Set a resolution with the 1:1 ratio and black bars around it
+            Screen.SetResolution(targetWidth, targetHeight, true);
+        }
+        else
+        {
+            // Return to windowed mode at 720x720
+            Screen.SetResolution(targetWidth, targetHeight, false);
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
+    }
+
 }
